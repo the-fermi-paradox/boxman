594f5b8a4b42771bec6cc8c6aa5d79a5f8477f03
fermi-paradox <86816389+the-fermi-paradox@users.noreply.github.com>
2023-11-22 20:10:20 -0600

    Adds basic code that puts image to window.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <SDL.h>
+#include <SDL2_image/SDL_image.h>
+
+#define SCREEN_WIDTH 2560
+#define SCREEN_HEIGHT 1600
 
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -9,8 +13,8 @@ int main() {
             "Boxman",
             SDL_WINDOWPOS_UNDEFINED,
             SDL_WINDOWPOS_UNDEFINED,
-            1080,
-            480,
+            SCREEN_WIDTH / 2,
+            SCREEN_HEIGHT / 2,
             SDL_WINDOW_SHOWN
     );
     if (window == NULL) {
@@ -22,7 +26,7 @@ int main() {
         printf("Surface failed to initialize. SDL Error: %s\n", SDL_GetError());
         return 1;
     }
-    if (SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0x00, 0x00, 0x00)) < 0) {
+    if (SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0x80, 0x80, 0x70)) < 0) {
         printf("Failed to fill rectangle. SDL Error: %s\n", SDL_GetError());
         return 1;
     }
@@ -30,8 +34,32 @@ int main() {
         printf("Failed to update window surface. SDL Error: %s\n", SDL_GetError());
         return 1;
     }
+    /* Initialize Image subsystem */
+    constexpr int img_flags = IMG_INIT_PNG;
+    if (!IMG_Init(IMG_INIT_PNG) & img_flags) {
+        printf("SDL2_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return 2;
+    }
+    SDL_Surface *hero = IMG_Load("/Users/lambda/sokoban/PNG/default/Crates/crate_01.png");
+    if (hero == NULL) {
+        printf("Failed to load hero sprite. SDL Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    printf("%d\n", hero->format->BitsPerPixel);
+    for (int i = 0; i < 3; ++i) {
+        SDL_Rect r = {100 * i, 100 * i, 100, 100};
+        if (SDL_BlitScaled(hero, NULL, screen_surface, &r) < 0) {
+            printf("Failed to Blit surface. SDL Error: %s\n", SDL_GetError());
+            return 1;
+        }
+    }
+    if (SDL_UpdateWindowSurface(window) < 0) {
+        printf("Failed to update window surface. SDL Error: %s\n", SDL_GetError());
+        return 1;
+    }
     SDL_Event e;
     bool quit = false;
+    /* Main game loop */
     while (!quit) {
         while (SDL_PollEvent(&e)) {
             switch (e.type) {
