623ae73cfb08363722a565e9ec5b7b82a05b9eca
fermi-paradox <86816389+the-fermi-paradox@users.noreply.github.com>
2023-11-23 14:25:31 -0600

    Adds render function to Texture

--- texture.h
+++ texture.h
@@ -8,6 +8,10 @@ public:
     Texture();
     Texture(SDL_Renderer *renderer, const char *path);
     ~Texture();
+    void Render(int x, int y) {
+       SDL_Rect r = {x, y, width, height};
+       SDL_RenderCopy(renderer, texture, nullptr, &r);
+    }
     SDL_Texture *texture;
     SDL_Renderer *renderer;
 private:
